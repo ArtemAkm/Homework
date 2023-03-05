c6548bbc70121f7921192b95e9723765681c6e36
Artem <akm.dnepr@gmail.com>
2023-03-05 16:38:15 +0200

    06.02

---
+++ 06.02/script.js
@@ -0,0 +1,15 @@
+.container .card .more{
+    padding: 10px 15px;
+    background: #17e78c;
+    color: #383838;
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 2px;
+    display: inline-block;
+    cursor: pointer;
+    margin-top: 15px;
+}
+.container .card .more:before{
+    content: 'Read More';
+}    
